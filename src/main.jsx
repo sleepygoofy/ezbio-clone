6e968ecfc1fe64e052310a5c5ab0129283b20a91
Amar Bajraktarevic <157768628+AmarBajraktarevic@users.noreply.github.com>
2024-02-25 00:46:42 -0500

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,20 +1,19 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client'; 
+import ReactDOM from 'react-dom'; // Correct import statement
 import { BrowserRouter, Routes, Route } from 'react-router-dom'; 
 import Home from './homepage.jsx';
 import App from './pages/shadow.jsx';
 import Night from './pages/night.jsx';
 
-
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} /> {}
-        <Route path="/shadow" element={<App />} /> {}
-        <Route path="/owner" element={<App />} /> {}
-        <Route path="/shdw" element={<App />} /> {}
-        <Route path="/night" element={<Night />} /> {}
+        <Route path="/" element={<Home />} />
+        <Route path="/shadow" element={<App />} />
+        <Route path="/owner" element={<App />} />
+        <Route path="/shdw" element={<App />} />
+        <Route path="/night" element={<Night />} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
