c084d7dbe9178bf1d3a44e4ddb870f919dc6751c
Amar Bajraktarevic <157768628+AmarBajraktarevic@users.noreply.github.com>
2024-02-25 00:56:06 -0500

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -11,8 +11,6 @@ ReactDOM.createRoot(document.getElementById('root')).render(
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/shadow" element={<App />} />
-        <Route path="/owner" element={<App />} />
-        <Route path="/shdw" element={<App />} />
         <Route path="/night" element={<Night />} />
       </Routes>
     </BrowserRouter>
