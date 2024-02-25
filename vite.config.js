93330989bef2b62f2c17b7806959ef4e206be3eb
Amar Bajraktarevic <157768628+AmarBajraktarevic@users.noreply.github.com>
2024-02-25 00:37:41 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import { resolve } from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
-    alias: {
-      // Map 'react-router-dom' to its actual location
-      'react-router-dom': 'react-router-dom'
-    }
+    extensions: ['.js', '.jsx']
   }
 });
